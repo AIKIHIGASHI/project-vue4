dafddbc3127fc93a146b12202b54847cc7db4d53
AIKIHIGASHI <sheltie0805@gmail.com>
2020-09-26 10:58:46 +0900

    Header、Footerコンポーネントの作成

---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    Copyright ©2019 oo inc. All rights reserved.
+  </div>
+</template>
+
+<style scoped>
+  div {
+    text-align: center;
+    font-size: 12px;
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<style scoped>
+div {
+  text-align: center;
+}
+
+img {
+  width: 100px;
+}
+</style>
