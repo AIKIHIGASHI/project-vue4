96e60b28d1b11cb850c03a937ce18606bbe3106b
AIKIHIGASHI <sheltie0805@gmail.com>
2020-10-08 18:54:05 +0900

    ユーザーの確認

--- src/App.vue
+++ src/App.vue
@@ -9,25 +9,35 @@
 <script>
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
+import firebase from 'firebase/app'
+import 'firebase/auth'
 export default {
   components: {
     Header,
     Footer
+  },
+  created() {
+    // ユーザーの確認
+    firebase.auth().onAuthStateChanged(user => {
+      if (!user) {
+        console.log('ユーザーはいません');
+        return;
+      }
+      console.log(user);
+    });
   }
 }
 </script>
 
 <style lang="scss">
-
-  #main {
-    margin: 40px;
-    text-align: center;
-    table {
-        margin: 30px auto;
-        th {
-          text-align: center;
-        }
-      }
-  } 
-
+#main {
+  margin: 40px;
+  text-align: center;
+  table {
+    margin: 30px auto;
+    th {
+      text-align: center;
+    }
+  }
+} 
 </style>
