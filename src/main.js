6afd2f7af252e82a178012d29b186820648cd1b2
AIKIHIGASHI <sheltie0805@gmail.com>
2020-10-08 18:53:39 +0900

    ユーザーの新規登録機能の実装
    ・メールアドレスとパスワード、ユーザ名を入力して新規登録

--- src/main.js
+++ src/main.js
@@ -2,8 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase/app'
+import "firebase/firestore"
+import "firebase/auth"
 
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC0M6c3UL8wORLCYOEEH9U47HeN057feQ0",
+  authDomain: "project-vue4.firebaseapp.com",
+  databaseURL: "https://project-vue4.firebaseio.com",
+  projectId: "project-vue4",
+  storageBucket: "project-vue4.appspot.com",
+  messagingSenderId: "316057063276",
+  appId: "1:316057063276:web:19028f49b5627aef2c6d49",
+  measurementId: "G-G94WGWH9GV"
+};
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,35 +1,59 @@
 <template>
   <div class="home">
     <div id="main">
-      
-        <h1 class="is-size-3">新規登録画面</h1>
-        <form>
-          <table>
-            <tbody>
-              <tr>
-                <th>ユーザ名</th>
-                <td><input type="text" placeholder="userName"></td>
-              </tr>
-              <tr>
-                <th>メールアドレス</th>
-                <td><input type="text" placeholder="E-mail"></td>
-              </tr>
-              <tr>
-                <th>パスワード</th>
-                <td><input type="text" placeholder="Password"></td>
-              </tr>
-            </tbody>
-          </table>
-        </form>
-      <span class="button is-info is-outlined">新規登録</span>
+      <h1 class="is-size-3">新規登録画面</h1>
+      <div class="has-text-danger">{{error}}</div>
+        <table>
+          <tbody>
+            <tr>
+              <th>ユーザ名</th>
+              <td><input type="text" placeholder="userName" v-model="userName"></td>
+            </tr>
+            <tr>
+              <th>メールアドレス</th>
+              <td><input type="email" placeholder="E-mail" v-model="email"></td>
+            </tr>
+            <tr>
+              <th>パスワード</th>
+              <td><input type="text" placeholder="Password" v-model="password"></td>
+            </tr>
+          </tbody>
+        </table>
+      <button class="button is-info is-outlined" @click='signUp()'>新規登録</button>
       <div><router-link class="has-text-info" to="/">ログインはこちらから</router-link></div>
     </div>
   </div>
 </template>
 <script>
-
-
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
 export default {
-
+  data() {
+    return {
+      userName: '',
+      email: '',
+      password: '',
+      error: ''
+    }
+  },
+  methods: {
+    signUp() {
+      if (!this.userName) {
+        this.error = 'The userName is blank';
+        return;
+      }
+      this.createUser();
+    },
+    createUser() {
+      firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+      .then(result => {
+        result.user.updateProfile({displayName: this.userName});
+      })
+      .catch(error => {
+        this.error = error.message;
+      });
+    }
+  }
 }
 </script>
