af6b5a75d9bfc54bd037d1327f334d3abe0550ac
AIKIHIGASHI <sheltie0805@gmail.com>
2020-10-14 06:39:11 +0900

    ダッシュボードにてログイン中ユーザの名前、ウォレットを表示する処理

--- src/store/index.js
+++ src/store/index.js
@@ -2,42 +2,95 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import router from '../router'
 import firebase from 'firebase/app'
+import 'firebase/firestore'
 import 'firebase/auth'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    loginUser: {},
+    loginUserStatus: {},
     error: ''
   },
   getters: {
+    loginUserName: state => state.loginUserStatus.name,
+    loginUserWallet: state => state.loginUserStatus.wallet,
     error: state => state.error
   },
   mutations: {
+    setLoginUser(state, user) {
+      state.loginUser = user;
+    },
+    getUsers(state, users) {
+      // ログイン中のuidでusersコレクション内からそのuidと一致するものを抽出し、ログイン中ユーザのdocをstate.loginUserStatusに入れる
+      state.loginUserStatus = users.find(({ uid }) => uid === state.loginUser.uid);
+    },
     errorMessage(state, error) {
       state.error = error;
     } 
   },
   actions: {
-    signUp({ commit }, { userName, email, password }) {
+    signUp({ commit, dispatch }, { userName, email, password }) {
       if (!userName) {
         commit('errorMessage', 'The userName is blank');
         return;
       }
       firebase.auth().createUserWithEmailAndPassword(email, password)
       .then(result => {
-        result.user.updateProfile({ displayName: userName });
-        commit('errorMessage', '');
-        router.push({ name: 'dashboard'});
+        result.user.updateProfile({ displayName: userName })
+        .then(async () => {
+          dispatch('addUser', userName);
+          await dispatch('getUsers', userName);
+          commit('errorMessage', '');
+          router.push({ name: 'dashboard'});
+        });
       })
       .catch(error => {
         commit('errorMessage', error);
       });
     },
+    // 現在ログインしているユーザの監視。ユーザがいればstate.loginUserにユーザの情報を入れる
+    setLoginUser({ dispatch, commit }) {
+      return new Promise(resolve => {
+        firebase.auth().onAuthStateChanged(async user => {
+          if (!user) {
+            return;
+          }
+          commit('setLoginUser', user);
+          await dispatch('getUsers');
+          resolve();
+        });
+      });
+    },
+     // サインアップ時にfirestoreにusersコレクションを作成。ドキュメントにそのユーザのuidを持たせる
+    addUser({ state }, userName) {
+      const db = firebase.firestore();
+      const collection = db.collection('users').doc(state.loginUser.uid);
+      collection.set({
+        uid: state.loginUser.uid,
+        name: userName,
+        wallet: 500
+      })
+    },
+    // firestoreのusersコレクションを取得
+    getUsers({ commit }) {
+      return new Promise (resolve => {
+        const db = firebase.firestore();
+        const collection = db.collection('users');
+        const users = [];
+        collection.get()
+        .then(snapshot => {
+          snapshot.forEach(doc => {
+            users.push(doc.data());
+          });
+          commit('getUsers', users);
+          resolve();
+        });
+      });
+    },
     login({ commit }, { email, password }) {
       firebase.auth().signInWithEmailAndPassword(email, password)
       .then(() => {
-        // 確認用
-        console.log('ログイン成功しました');
         commit('errorMessage', '');
         router.push({ name: 'dashboard'});
       })
